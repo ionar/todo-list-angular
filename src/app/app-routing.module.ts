be9f24c020650b1e87231f6de72dba0066a5972d
livia-tanaka <livia.tanaka@hotmail.com>
2023-11-15 13:56:52 -0300

    feat: estrutura HTML basica da listagem de task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ListTaskComponent } from './modules/task/components/list-task/list-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'list-user', component: ListTaskComponent},
+  {path: '', component: ListTaskComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TaskComponent } from './modules/task/task.component';
+import { ListTaskComponent } from './modules/task/components/list-task/list-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent,
+    ListTaskComponent
   ],
   imports: [
     BrowserModule,
